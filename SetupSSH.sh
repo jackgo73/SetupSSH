ef397f7067fe44bf3df92794a26c1004c1a3ab12
Jack Gao <jackgo73@outlook.com>
2018-07-31 20:38:29 +0800

    update

--- SetupSSH.sh
+++ SetupSSH.sh
@@ -1,3 +1,54 @@
 #!/bin/sh
 # Gaomingjie - Aug 2018
 
+numagrs=$#
+i=1
+
+HELP=false
+USR=$USER
+
+LOGFILE=/tmp/SetupSSH_`date +%F-%H-%M-%S`.log
+
+while [ $i -le $numargs ]; do
+	echo 'a'
+done
+
+# while [ $i -le $numargs ]; do
+#   j=$1 
+  # if [ $j = "--host" ] || [ $j = "-h" ]; then
+  #    HOST=$2
+  #    shift 1
+  #    i=`expr $i + 1`
+  # fi
+
+  # if [ $j = "--user" ] || [ $j = "-u" ]; then
+  #    USR=$2
+  #    shift 1
+  #    i=`expr $i + 1`
+  # fi
+
+  # if [ $j = "--logfile" ] || [ $j = "-l" ]; then
+  #    LOGFILE=$2
+  #    shift 1
+  #    i=`expr $i + 1`
+  # fi
+
+  # if [ $j = "--help" ]; then
+  #    HELP=true
+  # fi
+
+  # i=`expr $i + 1`
+  # shift 1
+# done
+
+
+if [ $HELP = true ]; then
+cat < EOF 
+asdad
+sdfsfd
+ewrrew
+
+EOF
+fi
+
+
