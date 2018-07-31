ff5cf5954a6e8ff41550a80ae404fa3eb2b34c22
Jack Gao <jackgo73@outlook.com>
2018-07-31 12:58:42 +0000

    update

--- SetupSSH.sh
+++ SetupSSH.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Gaomingjie - Aug 2018
 
-numagrs=$#
+numargs=$#
 i=1
 
 HELP=false
@@ -9,46 +9,56 @@ USR=$USER
 
 LOGFILE=/tmp/SetupSSH_`date +%F-%H-%M-%S`.log
 
+
+
 while [ $i -le $numargs ]; do
-	echo 'a'
+  j=$1 
+  if [ $j = "--host" ] || [ $j = "-h" ]; then
+     HOST=$2
+     shift 1
+     i=`expr $i + 1`
+  fi
+
+  if [ $j = "--user" ] || [ $j = "-u" ]; then
+     USR=$2
+     shift 1
+     i=`expr $i + 1`
+  fi
+
+  if [ $j = "--logfile" ] || [ $j = "-l" ]; then
+     LOGFILE=$2
+     shift 1
+     i=`expr $i + 1`
+  fi
+
+  if [ $j = "--help" ]; then
+     HELP=true
+  fi
+
+  i=`expr $i + 1`
+  shift 1
 done
-
-# while [ $i -le $numargs ]; do
-#   j=$1 
-  # if [ $j = "--host" ] || [ $j = "-h" ]; then
-  #    HOST=$2
-  #    shift 1
-  #    i=`expr $i + 1`
-  # fi
-
-  # if [ $j = "--user" ] || [ $j = "-u" ]; then
-  #    USR=$2
-  #    shift 1
-  #    i=`expr $i + 1`
-  # fi
-
-  # if [ $j = "--logfile" ] || [ $j = "-l" ]; then
-  #    LOGFILE=$2
-  #    shift 1
-  #    i=`expr $i + 1`
-  # fi
-
-  # if [ $j = "--help" ]; then
-  #    HELP=true
-  # fi
-
-  # i=`expr $i + 1`
-  # shift 1
-# done
 
 
 if [ $HELP = true ]; then
-cat < EOF 
-asdad
-sdfsfd
-ewrrew
-
+cat << EOF 
+add
+some help
+here
+...
+$HOME
+...
 EOF
 fi
 
+if [ -d $LOGFILE ]; then
+  echo $LOGFILE is a directory, setting logfile to $LOGFILE/SetupSSH_date.log
+  LOGFILE=$LOGFILE/SetupSSH_`date +%F-%H-%M-%S`.log
+fi
+echo The output of this script is also logged into $LOGFILE | tee -a $LOGFILE
+
+if [ `echo $?` != 0 ]; then
+    echo Error writing to the logfile $LOGFILE, Exiting
+    exit 1
+fi
 
