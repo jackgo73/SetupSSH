14c83a1abe361595fb449e8a15eb4a6f3cca0cb6
Jack Gao <jackgo73@outlook.com>
2018-07-31 23:29:13 -0400

    remote ssh

--- SetupSSH.sh
+++ SetupSSH.sh
@@ -6,6 +6,12 @@ i=1
 
 HELP=false
 USR=$USER
+NEW_LOCAL_PAIR=false
+
+IDRSA=id_rsa
+BITS=1024
+KEYTYPE="rsa"
+PORT=22
 
 LOGFILE=/tmp/SetupSSH_`date +%F-%H-%M-%S`.log
 
@@ -25,8 +31,20 @@ while [ $i -le $numargs ]; do
      i=`expr $i + 1`
   fi
 
+  if [ $j = "--newlocalpair" ] || [ $j = "-n" ]; then
+     NEW_LOCAL_PAIR=true
+     shift 1
+     i=`expr $i + 1`
+  fi
+
   if [ $j = "--logfile" ] || [ $j = "-l" ]; then
      LOGFILE=$2
+     shift 1
+     i=`expr $i + 1`
+  fi
+
+  if [ $j = "--port" ] || [ $j = "-p" ]; then
+     PORT=$2
      shift 1
      i=`expr $i + 1`
   fi
@@ -42,37 +60,11 @@ done
 
 if [ $HELP = true ]; then
 cat << EOF 
-add
-some help
-here
-...
-$HOME
-...
+
+--newpair: the script will remove the old private/public local key files existing and create new ones
+
 EOF
 fi
-
-#!------------------------------------!#
-#                func                  #
-#!------------------------------------!#
-log() {
-    echo -e "[LOG]$*" | tee -a $LOGFILE
-}
-
-logfailed() {
-    echo -e "\033[33m[LOG]$*\033[0m" | tee -a $LOGFILE
-}
-
-logsuccess() {
-    echo -e "\033[34m[LOG]$*\033[0m" | tee -a $LOGFILE
-}
-
-#!------------------------------------!#
-#               check                  #
-#!------------------------------------!#
-test -n "$HOST" || { logfailed "host is missing, use -h or --host to specify"; exit 1; }
-test -n "$USR"  || { logfailed "user is missing, use -u or --user to specify"; exit 1; }
-
-
 #!------------------------------------!#
 #                log                   #
 #!------------------------------------!#
@@ -80,11 +72,16 @@ if [ -d "$LOGFILE" ]; then
   echo "$LOGFILE is a directory, setting logfile to $LOGFILE/SetupSSH_\`data\`.log"
   LOGFILE=$LOGFILE/SetupSSH_`date +%F-%H-%M-%S`.log
 fi
-log "The output of this script is also logged into $LOGFILE" 
+echo "The output of this script is also logged into $LOGFILE" | tee -a $LOGFILE
 if [ $? != 0 ]; then
-    logfailed "Error writing to the logfile $LOGFILE, Exiting"
+    echo "Error writing to the logfile $LOGFILE, Exiting" | tee -a $LOGFILE
     exit 1
 fi
+#!------------------------------------!#
+#               check                  #
+#!------------------------------------!#
+test -n "$HOST" || { echo "host is missing, use -h or --host to specify" | tee -a $LOGFILE; exit 1; }
+test -n "$USR"  || { echo "user is missing, use -u or --user to specify" | tee -a $LOGFILE; exit 1; }
 #!------------------------------------!#
 #            check bins                #
 #!------------------------------------!#
@@ -92,19 +89,19 @@ MISSING=false
 
 SSH=`which ssh`
 test -z $SSH_PATH || SSH=$SSH_PATH
-command -v $SSH 1>/dev/null 2>&1 || { logfailed "ssh not found, please set the variable SSH_PATH"; MISSING=true; }
+command -v $SSH 1>/dev/null 2>&1 || { echo "ssh not found, please set the variable SSH_PATH" | tee -a $LOGFILE; MISSING=true; }
 
 SCP=`which scp`
 test -z $SCP_PATH || SCP=$SCP_PATH
-command -v $SCP 1>/dev/null 2>&1 || { logfailed "scp not found, please set the variable SCP_PATH"; MISSING=true; }
+command -v $SCP 1>/dev/null 2>&1 || { echo "scp not found, please set the variable SCP_PATH" | tee -a $LOGFILE; MISSING=true; }
 
 SSH_KEYGEN=`which ssh-keygen`
 test -z $SSH_KEYGEN_PATH || SSH_KEYGEN=$SSH_KEYGEN_PATH
-command -v $SSH_KEYGEN 1>/dev/null 2>&1 || { logfailed "ssh-keygeb not found, please set the variable SSH_KEYGEN_PATH"; MISSING=true; }
+command -v $SSH_KEYGEN 1>/dev/null 2>&1 || { echo "ssh-keygeb not found, please set the variable SSH_KEYGEN_PATH" | tee -a $LOGFILE; MISSING=true; }
 
 PING=`which ping`
 test -z $PING_PATH || PING=$PING_PATH
-command -v $PING 1>/dev/null 2>&1 || { logfailed "ping not found, please set the variable PING_PATH"; MISSING=true; }
+command -v $PING 1>/dev/null 2>&1 || { echo "ping not found, please set the variable PING_PATH" | tee -a $LOGFILE; MISSING=true; }
 
 $MISSING && { exit 1; }
 #!------------------------------------!#
@@ -113,18 +110,71 @@ $MISSING && { exit 1; }
 platform=`uname -s`
 case "$platform" in
   "Linux")  os=linux;;
-        *)  logfailed "$platform is not supported"
+        *)  echo "$platform is not supported" | tee -a $LOGFILE
             exit 1;;
 esac
-log "Platform: $platform "
+echo "Platform: $platform " | tee -a $LOGFILE
 #!------------------------------------!#
 #          check reachable             #
 #!------------------------------------!#
-$PING -c 5 -w 5 $HOST
+echo "---------------------------------------------" | tee -a $LOGFILE
+$PING -c 5 -w 5 $HOST                           | tee -a $LOGFILE
+echo "---------------------------------------------" | tee -a $LOGFILE
 if [ $? = 0 ]; then
-	logsuccess "Remote host reachability check succeeded, $HOST are reachable"
+	echo "Remote host reachability check succeeded, $HOST are reachable" | tee -a $LOGFILE
 else
-	logfailed "Remote host reachability check failed, $HOST are not reachable"
+	echo "Remote host reachability check failed, $HOST are not reachable" | tee -a $LOGFILE
 	exit
+fi
+#!------------------------------------!#
+#             local pair               #
+#!------------------------------------!#
+mkdir -p $HOME/.ssh                                                  | tee -a $LOGFILE
+touch $HOME/.ssh/authorized_keys                                     | tee -a $LOGFILE
+chmod 644 $HOME/.ssh/authorized_keys                                 | tee -a $LOGFILE
+mv -f $HOME/.ssh/authorized_keys $HOME/.ssh/authorized_keys.tmp      | tee -a $LOGFILE
+touch $HOME/.ssh/known_hosts                                         | tee -a $LOGFILE
+chmod 644 $HOME/.ssh/known_hosts                                     | tee -a $LOGFILE 
+mv -f $HOME/.ssh/known_hosts $HOME/.ssh/known_hosts.tmp              | tee -a $LOGFILE
+echo "Host *" > $HOME/.ssh/config.tmp                                | tee -a $LOGFILE
+echo "ForwardX11 no" >> $HOME/.ssh/config.tmp                        | tee -a $LOGFILE
+if test -f $HOME/.ssh/config; then
+	cp -f $HOME/.ssh/config $HOME/.ssh/config.backup                 | tee -a $LOGFILE
+fi
+mv -f $HOME/.ssh/config.tmp $HOME/.ssh/config                        | tee -a $LOGFILE
+chmod 644 $HOME/.ssh/config                                          | tee -a $LOGFILE
+
+if [ $NEW_LOCAL_PAIR = true ]; then
+	echo "Remove old key pair on local host"                         | tee -a $LOGFILE
+    rm -f $HOME/.ssh/${IDRSA}                                        | tee -a $LOGFILE
+    rm -f $HOME/.ssh/${IDRSA}.pub                                    | tee -a $LOGFILE
+    echo "Run ssh-keygen on local host with empty passphrase"        | tee -a $LOGFILE
+    echo "---------------------------------------------" | tee -a $LOGFILE
+    $SSH_KEYGEN -t $KEYTYPE -b $BITS -f $HOME/.ssh/${IDRSA} -N ''    | tee -a $LOGFILE
+    echo "---------------------------------------------" | tee -a $LOGFILE
+elif [ -f $HOME/.ssh/${IDRSA}.pub ] && [ -f $HOME/.ssh/${IDRSA} ]; then
+	echo "Use the local key pair that already exists"                | tee -a $LOGFILE
+	continue
+else
+	echo "Key pair is missing, create new one on local host"         | tee -a $LOGFILE
+    rm -f $HOME/.ssh/${IDRSA}                                        | tee -a $LOGFILE
+    rm -f $HOME/.ssh/${IDRSA}.pub                                    | tee -a $LOGFILE
+    echo "Run ssh-keygen on local host with empty passphrase"        | tee -a $LOGFILE
+    echo "---------------------------------------------" | tee -a $LOGFILE
+    $SSH_KEYGEN -t $KEYTYPE -b $BITS -f $HOME/.ssh/${IDRSA} -N ''    | tee -a $LOGFILE
+    echo "---------------------------------------------" | tee -a $LOGFILE
+fi
+#!------------------------------------!#
+#            remote pair               #
+#!------------------------------------!#
+echo "Creating .ssh directory and setting permissions on remote host $host" | tee -a $LOGFILE
+echo "The user may be prompted for a password here"                         | tee -a $LOGFILE
+echo "---------------------------------------------" | tee -a $LOGFILE
+$SSH -p $PORT -o StrictHostKeyChecking=no -x -l $USR $HOST "/bin/sh -c \"  mkdir -p .ssh ; chmod og-w . .ssh;   touch .ssh/authorized_keys .ssh/known_hosts;  chmod 644 .ssh/authorized_keys  .ssh/known_hosts; cp  .ssh/authorized_keys .ssh/authorized_keys.tmp ;  cp .ssh/known_hosts .ssh/known_hosts.tmp; echo \\"Host *\\" > .ssh/config.tmp; echo \\"ForwardX11 no\\" >> .ssh/config.tmp; if test -f  .ssh/config ; then cp -f .ssh/config .ssh/config.backup; fi ; mv -f .ssh/config.tmp .ssh/config\""
+echo "---------------------------------------------" | tee -a $LOGFILE
+if [ $? -eq 0 ]; then
+	echo "Done with creating .ssh directory and setting permissions on remote host $host" | tee -a $LOGFILE
+else
+	exit 1
 fi
 
